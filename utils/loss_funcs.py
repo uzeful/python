90c51ba2b7b31273dd52368f28df9d6127714757
uzeful <uzeful@163.com>
2017-07-10 21:48:59 +0800

    loss_func

--- utils/loss_funcs.py
+++ utils/loss_funcs.py
@@ -1,11 +1,9 @@
 import torch
-
-'''
-Refered to the project of Zengyu714
-'''
+import pdb
 
 
-def dice_loss(output, target):
+
+def dice_loss_bak(output, target):
     """Compute dice among **positive** labels to avoid unbalance.
 
     Arguments:
@@ -20,20 +18,24 @@ def dice_loss(output, target):
         + predtrue: FloatTensor {0.0, 1.0} with shape [batch_size * height * width, (1)]
         + true: FloatTensor {0.0, 1.0} with shape [batch_size * height * width, (1)]
     """
-    predict = output.max(1)[1]  # {0, 1}. 0 for the original output, 1 for the binary mask
-    target = target.float()
+
+    pdb.set_trace()
+
+    predict = (output.max(1)[1]).float()  # {0, 1}. 0 for the original output, 1 for the binary mask
+    target = (target.squeeze(1)).float()
 
     # Loss
     intersection = torch.sum(predict * target, 0)
     union = torch.sum(predict * target, 0) + torch.sum(target * target, 0)
-    dice = 2.0 * intersection / union
+    dice = 2.0 * intersection / (union + 1e-7)
+    loss = 1 - dice
 
     # Overlap
     predtrue = predict.eq(1).float().data    # FloatTensor 0.0 / 1.0
     true = target.data                  # FloatTensor 0.0 / 1.0
-    overlap = 2 * (predtrue * true).sum() / (predtrue.sum() + true.sum()) * 100
+    overlap = 2 * (predtrue * true).sum() / (predtrue.sum() + true.sum() + 1e-7) * 100
 
     # Accuracy
     acc = predtrue.eq(true).float().mean()
-    return 1 - torch.clamp(dice, 0.0, 1.0 - 1e-7), acc, overlap, predtrue, true
-
+    #return 1 - torch.clamp(dice, 0.0, 1.0 - 1e-7), acc, overlap, predtrue, true
+    return loss, acc, overlap

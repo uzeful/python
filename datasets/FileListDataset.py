1dde9129578c1be3eef7d7792afcc1d36bb02a22
Yu Zhang <uzeful@163.com>
2017-10-27 17:06:20 +0800

    Update FileListDataset.py

--- datasets/FileListDataset.py
+++ datasets/FileListDataset.py
@@ -5,7 +5,6 @@ File list Dataset Preparation
 import torch.utils.data as data
 from PIL import Image
 import os
-#import os.path
 
 def default_loader(path):
     return Image.open(path).convert('RGB')
